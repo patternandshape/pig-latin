e8dd7fc4086fe64fd6a0a44de041a2e252eed95b
Alex Larson and Molly Curtin <alex.m.larson@gmail.com>
2016-01-20 14:55:48 -0800

    add working html and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,26 @@ var isVowel = function(word) {
 };
 
 var isCon = function(word) {
-   if (isVowel(word) === false ) {
+  if (isVowel(word) === false) {
     var wordArray = word.split('');
     var shiftArray = wordArray.shift();
     wordArray.push((shiftArray) + "ay");
+    return wordArray.join("");
+  };
+};
+
+var isVowel2 = function(word) {
+  var wordArray = word.split('');
+  return (/^[aeiou]$/i).test(wordArray[1]);
+};
+
+var isDoubleCon = function(word) {
+  if (isVowel(word) || isVowel2(word) === false) {
+    var wordArray = word.split('');
+    var shiftArray = wordArray.shift();
+    var secondArray = wordArray.shift();
+    wordArray.push(shiftArray);
+    wordArray.push((secondArray) + "ay");
     return wordArray.join("");
   };
 };
@@ -21,24 +37,8 @@ var isQuTwo = function(word) {
   return (/^[q]$/i).test(wordArray[1]);
 };
 
-var isVowel2 = function(word) {
-  var wordArray = word.split('');
-  return (/^[aeiou]$/i).test(wordArray[1]);
-};
-
-var isDoubleCon = function(word) {
-    if (isVowel(word) || isVowel2(word) === false) {
-    var wordArray = word.split('');
-    var shiftArray = wordArray.shift();
-    var secondArray = wordArray.shift();
-    wordArray.push(shiftArray);
-    wordArray.push((secondArray) + "ay");
-    return wordArray.join("");
-  };
-};
-
 var hasQuFirst = function(word) {
-   if (isQu(word) === true) {
+  if (isQu(word) === true) {
     var wordArray = word.split('');
     var shiftArray = wordArray.shift();
     var shiftSecond = wordArray.shift();
@@ -49,7 +49,7 @@ var hasQuFirst = function(word) {
 };
 
 var hasQuSecond = function(word) {
-    if (isQuTwo(word) === true) {
+  if (isQuTwo(word) === true) {
     var wordArray = word.split('');
     var shiftArray = wordArray.shift();
     var secondArray = wordArray.shift();
@@ -61,21 +61,47 @@ var hasQuSecond = function(word) {
     return wordArray.join("");
   };
 };
-    var pigLatinMaker = function (word);
+var plMaker = function(word) {
 
-{
+  if (isVowel(word) === true) {
+    var word = (word + "ay");
+    return word;
+  } else if (isVowel(word) === false) {
+    var word = isCon(word);
+    return word;
 
+  } else if (isVowel2(word) === true) {
+    var word = isCon(word);
+    return word;
+
+  } else if (isVowel2(word) === false) {
+    var word = isDoubleCon(word);
+    return word;
+
+
+  } else if (isQu(word) === true) {
+    var word = hasQuFirst(word);
+    return word;
+
+
+  } else if (isQu(word) === false) {
+    var word = hasQuSecond(word);
+    return word;
+
+  } else {
+    var word = "Please speak English";
+    return word;
+  }
 }
 
 $(document).ready(function() {
 
   $("#pigForm").submit(function(event) {
-     var input = $(".userInput").val();
+    var input = $(".userInput").val();
+    var plo = plMaker(input);
+    $(".output").text(plo);
 
-var piglatinOutput = pigLatinMaker(input);
-$
+    event.preventDefault();
 
-   event.preventDefault();
-
-      })
-  });
+  })
+});
