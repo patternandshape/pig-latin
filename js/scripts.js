80b46a6065aedfe154dc54d85a84a3e62dd16c0e
Alex Larson and Molly Curtin <alex.m.larson@gmail.com>
2016-01-20 13:59:21 -0800

    add html functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ var isVowel = function(word) {
   return (/^[aeiou]$/i).test(wordArray[0]);
 };
 
+var isCon = function(word) {
+   if (isVowel(word) === false ) {
+    var wordArray = word.split('');
+    var shiftArray = wordArray.shift();
+    wordArray.push((shiftArray) + "ay");
+    return wordArray.join("");
+  };
+};
+
 var isQu = function(word) {
   var wordArray = word.split('');
   return (/^[q]$/i).test(wordArray[0]);
@@ -17,56 +26,56 @@ var isVowel2 = function(word) {
   return (/^[aeiou]$/i).test(wordArray[1]);
 };
 
-var isCon = function(word) {
- if (isVowel(word) === false ) {
-  var wordArray = word.split('');
-  var shiftArray = wordArray.shift();
-  wordArray.push((shiftArray) + "ay");
-  return wordArray.join("");
-};
-};
-
 var isDoubleCon = function(word) {
-  if (isVowel(word) || isVowel2(word) === false) {
-  var wordArray = word.split('');
-  var shiftArray = wordArray.shift();
-  var secondArray = wordArray.shift();
-  wordArray.push(shiftArray);
-  wordArray.push((secondArray) + "ay");
-  return wordArray.join("");
-};
+    if (isVowel(word) || isVowel2(word) === false) {
+    var wordArray = word.split('');
+    var shiftArray = wordArray.shift();
+    var secondArray = wordArray.shift();
+    wordArray.push(shiftArray);
+    wordArray.push((secondArray) + "ay");
+    return wordArray.join("");
+  };
 };
 
 var hasQuFirst = function(word) {
- if (isQu(word) === true) {
-  var wordArray = word.split('');
-  var shiftArray = wordArray.shift();
-  var shiftSecond = wordArray.shift();
-  wordArray.push(shiftArray);
-  wordArray.push((shiftSecond) + "ay");
-  return wordArray.join("");
-};
+   if (isQu(word) === true) {
+    var wordArray = word.split('');
+    var shiftArray = wordArray.shift();
+    var shiftSecond = wordArray.shift();
+    wordArray.push(shiftArray);
+    wordArray.push((shiftSecond) + "ay");
+    return wordArray.join("");
+  };
 };
 
 var hasQuSecond = function(word) {
-  if (isQuTwo(word) === true) {
-  var wordArray = word.split('');
-  var shiftArray = wordArray.shift();
-  var secondArray = wordArray.shift();
-  var thirdArray = wordArray.shift();
+    if (isQuTwo(word) === true) {
+    var wordArray = word.split('');
+    var shiftArray = wordArray.shift();
+    var secondArray = wordArray.shift();
+    var thirdArray = wordArray.shift();
 
-  wordArray.push(shiftArray);
-  wordArray.push(secondArray);
-  wordArray.push((thirdArray) + "ay");
-  return wordArray.join("");
+    wordArray.push(shiftArray);
+    wordArray.push(secondArray);
+    wordArray.push((thirdArray) + "ay");
+    return wordArray.join("");
+  };
 };
-};
+    var pigLatinMaker = function (word);
+
+{
+
+}
 
 $(document).ready(function() {
+
   $("#pigForm").submit(function(event) {
-    var input = $(".userInput").val();
-    console.log("hey");
-    $(".output").text(input);
-    event.preventDefault();
+     var input = $(".userInput").val();
+
+var piglatinOutput = pigLatinMaker(input);
+$
+
+   event.preventDefault();
+
+      })
   });
-});
